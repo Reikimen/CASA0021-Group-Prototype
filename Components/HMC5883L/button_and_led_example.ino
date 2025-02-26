7506c0e0c0ccac1c9b5f9134eadf4f64b3159cc3
Flora Shen <98469390+JY-SHENNNN@users.noreply.github.com>
2025-02-26 23:03:51 +0000

    Update button_and_led_example.ino

--- Components/HMC5883L/button_and_led_example.ino
+++ Components/HMC5883L/button_and_led_example.ino
@@ -1,63 +1,106 @@
 #include <Adafruit_NeoPixel.h>
+#include <esp_sleep.h>  // low power mode lib
 
 #define NUMPIXELS 24
-#define LED_PIN 15         //
-#define BUTTON_PIN 4     
+#define LED_PIN 15          // WS2812B 
+#define BUTTON_PIN1 4       // button1
+#define BUTTON_PIN2 18      // button2
+#define BUTTON_PIN3 19      // button3
+#define BUTTON_START 23     // Start button
 
-volatile bool buttonPressed = false; // press flag
-unsigned long lastInterruptTime = 0;
-int pressCount = 0;                  // count the number of press
+volatile bool button1Pressed = false;
+volatile bool button2Pressed = false;
+volatile bool button3Pressed = false;
+volatile bool startPressed = false;
+volatile int startPressCount = 0; // record start button pressed time
 
 Adafruit_NeoPixel pixels(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
     Serial.begin(9600);
 
-    pinMode(BUTTON_PIN, INPUT_PULLUP); // poll-up resistance involved
-    pinMode(LED_PIN, OUTPUT);
-    digitalWrite(LED_PIN, LOW); 
-
+    // pull-up resistance
+    pinMode(BUTTON_PIN1, INPUT_PULLUP);
+    pinMode(BUTTON_PIN2, INPUT_PULLUP);
+    pinMode(BUTTON_PIN3, INPUT_PULLUP);
+    pinMode(BUTTON_START, INPUT_PULLUP);
+    
     pixels.begin();
     pixels.setBrightness(10);
     pixels.clear();
     pixels.show();
 
-    // Interrupt
-    attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), handleButtonPress, FALLING);
+    // 4 buttons interrupt
+    attachInterrupt(digitalPinToInterrupt(BUTTON_PIN1), [](){ handleButtonPress(1); }, FALLING);
+    attachInterrupt(digitalPinToInterrupt(BUTTON_PIN2), [](){ handleButtonPress(2); }, FALLING);
+    attachInterrupt(digitalPinToInterrupt(BUTTON_PIN3), [](){ handleButtonPress(3); }, FALLING);
+    attachInterrupt(digitalPinToInterrupt(BUTTON_START), [](){ handleButtonPress(4); }, FALLING);
 }
 
 void loop() {
-    if (buttonPressed) {
-        buttonPressed = false; // clear flag
-        
-        if (pressCount == 1) {
-            lightUpPixels(0, 23, 255, 255, 255); 
-        } else if (pressCount == 2) {
-            lightUpPixels(4, 8, 255, 255, 0);
-        } else if (pressCount == 3) {
-            lightUpPixels(12, 16, 255, 255, 0); 
-        } else if (pressCount == 4){
-            pixels.clear();
-            pixels.show();
-            pressCount = 0; 
-        }    
+    if (button1Pressed) {
+        button1Pressed = false;
+        lightUpPixels(4, 8, 255, 255, 0); 
+        Serial.println("SAD");
+    } 
+    if (button2Pressed) {
+        button2Pressed = false;
+        lightUpPixels(12, 16, 255, 255, 0); 
+        Serial.println("HAPPY");
+    } 
+    if (button3Pressed) {
+        button3Pressed = false;
+        lightUpPixels(18, 24, 255, 255, 0); 
+        Serial.println("ANGRY");
+    }
+    
+    if (startPressed) {
+        startPressed = false;
+        startPressCount++;
+
+        if (startPressCount == 1) {
+            // first press
+            lightUpPixels(0, 23, 255, 255, 255); // turn on the white light
+            Serial.println("START: Lights ON");
+        } else if (startPressCount == 2) {
+            // second press
+            Serial.println("Entering Deep Sleep...");
+            delay(500); 
+            enterDeepSleep(); // sleep mode
+        }
     }
 }
 
-// button processed with interrupt
-void handleButtonPress() {
+
+void handleButtonPress(int buttonID) {
+    static unsigned long lastInterruptTime = 0;
     unsigned long currentTime = millis();
-    if (currentTime - lastInterruptTime > 200) { // debounce
-        buttonPressed = true;
-        pressCount++;
+
+    if (currentTime - lastInterruptTime > 200) { // 200ms debounce
+        switch (buttonID) {
+            case 1: button1Pressed = true; break;
+            case 2: button2Pressed = true; break;
+            case 3: button3Pressed = true; break;
+            case 4: startPressed = true; break;
+        }
         lastInterruptTime = currentTime;
     }
 }
 
-// light the led
+// turn on the light
 void lightUpPixels(int start, int end, int red, int green, int blue) {
+    pixels.clear();
     for (int i = start; i <= end; i++) {
         pixels.setPixelColor(i, pixels.Color(red, green, blue));
     }
     pixels.show();
+}
+
+// sleep mode
+void enterDeepSleep() {
+    pixels.clear();
+    pixels.show();
+    
+    Serial.println("ESP32 is going to sleep...");
+    esp_deep_sleep_start(); 
 }
